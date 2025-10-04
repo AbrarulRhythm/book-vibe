9b4563450c4fc18f8ac2a80810a3a8880722ebef
Abrarul Rhythm <[....email>
2025-10-05 00:01:00 +0600

    added error page and setup default page index

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { RouterProvider } from "react-router/dom";
-import { router } from './Routes/routes.jsx';
+import { RouterProvider } from "react-router";
+import { router } from './Routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
