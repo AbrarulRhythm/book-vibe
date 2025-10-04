dde53a94f06c6eddcb1f19fd8e57fe7adeeed0bf
Abrarul Rhythm <[....email>
2025-10-05 00:21:29 +0600

    added header

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
 const Navbar = () => {
+
+    const links = <>
+        <li className='mx-2 text-base font-medium'>Home</li>
+        <li className='mx-2 text-base font-medium'>About</li>
+    </>
+
     return (
-        <div>
-            <h1>Hello I'm Navbar</h1>
-        </div>
+        <header>
+            <nav className=''>
+                <div className='container'>
+                    <div className="navbar px-0 py-6 md:py-8 lg:py-10">
+                        <div className="navbar-start">
+                            <div className="dropdown">
+                                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                    {links}
+                                </ul>
+                            </div>
+                            <a href='/' className="text-[28px] font-bold hover:opacity-65 duration-300" title='Book Vibe'>Book Vibe</a>
+                        </div>
+                        <div className="navbar-center hidden lg:flex">
+                            <ul className="menu menu-horizontal px-1">
+                                {links}
+                            </ul>
+                        </div>
+                        <div className="navbar-end">
+                            <a className="btn">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 };
 
