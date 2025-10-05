ee2489516463f698fbe1cc01e292a76819a7590c
Abrarul Rhythm <[....email>
2025-10-06 01:28:29 +0600

    added sorted functionality

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -5,10 +5,12 @@ import 'react-tabs/style/react-tabs.css';
 import { getStoredBook } from '../../utility/addToDB';
 import Book from '../Book/Book';
 import SingleReadList from '../SingleReadList/SingleReadList';
+import { FaAngleDown } from 'react-icons/fa';
 
 const ReadList = () => {
 
     const [readList, setReadList] = useState([]);
+    const [sort, setSort] = useState('');
 
     const data = useLoaderData();
 
@@ -17,11 +19,33 @@ const ReadList = () => {
         const convertedStoredBook = storedBookData.map(id => parseInt(id));
         const myReadList = data.filter(book => convertedStoredBook.includes(book.bookId));
         setReadList(myReadList);
-    }, []);
+    }, [data]);
+
+    const handleSort = (type) => {
+        setSort(type)
+
+        if (type === 'pages') {
+            const sortedByPage = [...readList].sort((a, b) => a.totalPages - b.totalPages);
+            setReadList(sortedByPage);
+        }
+
+        if (type === 'ratings') {
+            const sortedByRatings = [...readList].sort((a, b) => a.rating - b.rating);
+            setReadList(sortedByRatings);
+        }
+    }
 
     return (
         <section className='read-list'>
             <div className='container'>
+                <div className='dropdown my-11'>
+                    <div tabIndex={0} role='button' className='bg-[#23BE0A] text-white py-4 px-6 rounded-lg font-semibold flex items-center cursor-pointer hover:bg-[#16a800] duration-300'>Sort By : {sort ? sort : ''} <FaAngleDown className='ml-1 text-xl' /></div>
+                    <ul tabIndex={0} className='dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm'>
+                        <li><a onClick={() => handleSort('pages')}>Pages</a></li>
+                        <li><a onClick={() => handleSort('ratings')}>Ratings</a></li>
+                    </ul>
+                </div>
+
                 <Tabs>
                     <TabList>
                         <Tab>Read Books</Tab>
